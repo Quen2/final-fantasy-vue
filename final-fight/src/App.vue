12c54c7c26b89dc31a96da6a7d1e16a7292994f8
QuentinG <ZexFrost1@gmail.com>
2026-02-05 11:43:13 +0100

    [+] First push, base structure with playerpannel

---
+++ final-fight/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-screen h-screen bg-black flex flex-col justify-center items-center">
+    <div class="flex flex-col w-3/4 h-3/4 bg-gray-600">
+      <div class="flex justify-between p-4">
+        <PlayerPannel max-hp="50" hp="50" name="Quentin" />
+        <PlayerPannel max-hp="60" hp="60" name="LARRY" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import PlayerPannel from "@/Components/PlayerPannel.vue";
+  export default {
+    components: {
+      PlayerPannel,
+    }
+  }
+</script>
+
+<style scoped></style>
---
+++ final-fight/src/Components/PlayerPannel.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex flex-col">
+    <p>{{ name }}</p>
+    <div class="flex">
+      {{ hp }} / {{ maxHp }} hp
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PlayerPannel',
+    props: {
+      name: {
+        type: String,
+        required: true
+      },
+      hp: {
+        type: Number,
+        required: true
+      },
+      maxHp: {
+        type: Number,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
